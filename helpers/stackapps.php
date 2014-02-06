e83d75c3b3dd546292b58dcea4f79eb60d241379
Makis Tracend <makis.tracend@gmail.com>
2014-02-05 19:08:39 -0800

    setting api name

--- helpers/stackapps.php
+++ helpers/stackapps.php
@@ -1,7 +1,8 @@
 <?php
 /* Discus for KISSCMS */
 class Stackapps {
-	
+
+	public $name = "stackapps";
 	public $key;
 	public $secret;
 	public $token;
@@ -10,19 +11,19 @@ class Stackapps {
 	public $me;
 	public $oauth;
 	private $cache;
-	
+
 	function  __construct() {
-		
+
 		$this->api = "https://stackapps.com/api/3.0/";
-		
+
 		$this->key = $GLOBALS['config']['stackapps']['key'];
 	 	$this->secret = $GLOBALS['config']['stackapps']['secret'];
-		
+
 		$this->me = ( empty($_SESSION['oauth']['stackapps']['user_id']) ) ? false : $_SESSION['oauth']['stackapps']['user_id'];
-	 	
+
 		$this->token = ( empty($_SESSION['oauth']['stackapps']['access_token']) ) ? false : $_SESSION['oauth']['stackapps']['access_token'];
 	 	$this->refresh_token = ( empty($_SESSION['oauth']['stackapps']['refresh_token']) ) ? false : $_SESSION['oauth']['stackapps']['refresh_token'];
-	 	
+
 		$this->cache = $this->getCache();
 		// check if we need to refresh the token
 		//$expires_in = $_SESSION['oauth']['stackapps']['expires_in']; // seconds
@@ -30,21 +31,21 @@ class Stackapps {
 			//$this->token = $_SESSION['oauth']['stackapps']['access_token'] = $this->refreshToken();
 		//}
 	}
-	
-	
+
+
 	function listThread( $id ){
 		$url = $this->api ."threads/listPosts.json?api_key=". $this->key ."&thread=".$id;
 		$http = new Http();
 		$http->execute( $url );
 		return ($http->error) ? die($http->error) : json_decode( $http->result);
 	}
-	
+
 	function listFollowing(){
 		// return the cache under conditions
 		if( $this->checkCache("following") ) return $this->cache['following'];
-		
+
 		$url = $this->api ."users/listFollowing.json?access_token=". $this->token ."&api_key=". $this->key ."&api_secret=". $this->secret ."&user=".$this->me;
-		
+
 		$http = new Http();
 		$http->execute( $url );
 		$result = ($http->error) ? die($http->error) : json_decode( $http->result);
@@ -66,32 +67,32 @@ class Stackapps {
 		$http = new Http();
 		$http->setMethod('POST');
 		$http->setParams( array(
-				"access_token" => $this->token, 
-				"api_key" => "$this->key", 
-				"api_secret" => "$this->secret", 
-				"parent" => $id, 
+				"access_token" => $this->token,
+				"api_key" => "$this->key",
+				"api_secret" => "$this->secret",
+				"parent" => $id,
 				"message" => $message,
-			) 
+			)
 		);
 		$http->execute( $url );
-		
+
 		return ($http->error) ? die($http->error) : json_decode( $http->result);
-		
+
 	}
-	
+
 	function createReply( $data ){
-		
+
 			$comment = new Comment( $data->id, "stackapps");
 			$user = new User(0, "stackapps");
 			$discussion = new Discussion(0 , "stackapps");
-			
+
 			if( !empty($data->thread->id) ) $discussion->set('api_id', $data->thread->id);
 			if( !empty($data->thread->title) ) $discussion->set('title', $data->thread->title);
 			if( !empty($data->thread->message) ) $discussion->set('content', $data->thread->message);
 			if( !empty($data->thread->link) ) $discussion->set('link', $data->thread->link);
 			// find the unique id
 			$discussion->set('id', $discussion->getID() );
-			
+
 			if( !empty($data->author->id) ) $user->set('api_id', $data->author->id);
 			if( !empty($data->author->name) ) $user->set('name', $data->author->name);
 			if( !empty($data->author->about) ) $user->set('about', $data->author->about);
@@ -99,7 +100,7 @@ class Stackapps {
 			if( !empty($data->author->avatar->permalink) ) $user->set('avatar', $data->author->avatar->permalink);
 			// find the unique id
 			$user->set('id', $user->getID() );
-			
+
 			if( !empty($data->createdAt) ) $comment->set('date', $data->createdAt);
 			// we want the comment tripped out from any formatting
 			if( !empty($data->message) ) $comment->set('message', strip_tags( htmlspecialchars_decode( $data->message )) );
@@ -109,18 +110,18 @@ class Stackapps {
 			// link comment with discussion/user
 			$comment->set('discussion', $discussion->get("id") );
 			$comment->set('user', $user->get("id") );
-			
-			
+
+
 			$is_following = $this->isFollowing( $user->get("api_id") );
 			$is_mine = $this->isMine( $user->get("api_id") );
 			// check if following the discussion
 			$discussion->set('is_following', ( $is_following || $is_mine ) );
-			// check if following the user 
+			// check if following the user
 			$user->set('is_following', $is_following );
-			// check if the comment ownership 
+			// check if the comment ownership
 			$comment->set('is_mine', $is_mine );
-			
-			
+
+
 			// compile entry
 			$reply = array(
 				"id" => $comment->get("id"),
@@ -128,18 +129,18 @@ class Stackapps {
 				"user" => $user->rs,
 				"discussion" => $discussion->rs,
 			);
-			
+
 			return $reply;
 	}
-	
-	
+
+
 	function getCache(){
 		// set up the parent container, the first time
 		if( !array_key_exists("stackapps", $_SESSION) ) $_SESSION['stackapps']= array();
 		return $_SESSION['stackapps'];
-		
+
 	}
-	
+
 	function setCache( $data ){
 		// save the data in the session
 		foreach( $data as $key => $result ){
@@ -148,22 +149,22 @@ class Stackapps {
 		// update the local variable
 		$this->cache = $this->getCache();
 	}
-	
+
 	function checkCache( $type ){
 		// always discard cache on debug mode
-		if( DEBUG ) return false; 
-		
+		if( DEBUG ) return false;
+
 		if( !empty($this->cache[$type]) ) {
-			// check the date 
+			// check the date
 			$valid = true;
 		}
-		
+
 		return ( $valid ) ? true : false;
 	}
-	
+
 	function deleteCache(){
 		unset($_SESSION['stackapps']);
 	}
-		
-	
+
+
 }
